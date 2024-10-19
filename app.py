4ae23f0b5c752cbc51003108cdfc904685b19787
s2pdeveloper <124123282+s2pdeveloper@users.noreply.github.com>
2024-10-19 17:19:17 +0530

    changes

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from fastapi.staticfiles import StaticFiles
 from utils.exception import CustomExceptionHandler
 from models.creation import TableCreation
 app = FastAPI(swagger_ui_parameters={"displayRequestDuration": True})
-app.mount("/", StaticFiles(directory="dist", html=True), name="dist")
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -16,12 +16,19 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+# app.mount("/static", StaticFiles(directory="dist", html=True), name="static")
 
+# # Optional: Redirect from `/` to `/static/index.html`
+# @app.get("/")
+# async def root():
+#     return RedirectResponse(url="/static/index.html")
 app.add_middleware(CustomExceptionHandler)
 init_db()
 app.include_router(chat_bot.router,
     prefix="/chat-bot",
     tags=["ChatBot"])
+app.mount("/", StaticFiles(directory="dist", html=True), name="dist")
+
 
 # handler = Mangum(app)
 
